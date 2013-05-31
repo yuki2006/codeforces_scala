b4cac4c9aadd4aeddf2615d3ff78a7f5b3c01836
yuki2006 <yagfair@gmail.com>
2013-05-31 15:57:42 +0900

    p312Bのコードを短くしました。

--- src/P312B.scala
+++ src/P312B.scala
@@ -1,12 +1,8 @@
 object P312B extends App {
   var str = readLine();
-  var data_str = str.split(" ");
-  var datas = new Array[Integer](4);
-  for (i <- 0 to 3) {
-    datas.update(i, data_str(i).toInt);
-  }
-  var p = Array(1.0 * datas(0) / datas(1),1.0 * datas(2) / datas(3));
+  var data = str.split(" ");
+  var p = Array(data(0).toDouble / data(1).toDouble, data(2).toDouble / data(3).toDouble);
   //無限等比数列でした。。
-  var answer =p(0)/(1-(1-p(0))*(1-p(1)))
+  var answer = p(0) / (1 - (1 - p(0)) * (1 - p(1)))
   print(answer);
 }
